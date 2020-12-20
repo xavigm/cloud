6c23e072c7e2ae05776150af812e548492ba04e2
Xavi <xavi.coupe@gmail.com>
2020-12-20 23:48:50 +0100

    pushed site code

---
+++ wp-content/plugins/blossomthemes-email-newsletter/public/js/blossomthemes-email-newsletter-public.min.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function($){
+	// $('body').on('click', '.bten-del-icon', function() {
+ //        $(this).parent().parent().fadeOut('slow', function() {
+ //            $(this).remove();
+ //        });
+ //        return;
+ //    });
+});
